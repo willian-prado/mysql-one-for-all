7785e59a52194978b44c870621264ea01b743f27
Willian Prado <willian_prado@hotmail.com>
2021-10-01 19:57:12 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+	BEFORE DELETE ON SpotifyClone.User
+	FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.User_Artist WHERE user_id = OLD.id; 
+	DELETE FROM SpotifyClone.User_Song WHERE user_id = OLD.id;
+END
+$$
+
+DELIMITER ;
