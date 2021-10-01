e763e86bff0b6c613a51ac91f2a97a98057f2303
Willian Prado <willian_prado@hotmail.com>
2021-10-01 20:06:37 -0300

    Corrige codeclimate do desafio9.sql

--- desafio9.sql
+++ desafio9.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE albuns_do_artista(
     IN artist_name VARCHAR(50)
 )
 BEGIN
-	SELECT a.name AS `artista`, al.title AS `album` FROM SpotifyClone.Album AS al
+  SELECT a.name AS `artista`, al.title AS `album` FROM SpotifyClone.Album AS al
 	INNER JOIN SpotifyClone.Artist AS a ON a.id = al.artist_id
 	WHERE a.name = artist_name;
 END $$
