ce8b6f5655a0381f2a552557eec65c41240387ba
Willian Prado <willian_prado@hotmail.com>
2021-10-01 19:57:33 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id_user int)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE songs_history INT;
+    SELECT 
+    COUNT(user_id) AS `quantidade_musicas_no_historico`
+	FROM
+		SpotifyClone.User_Song
+	WHERE
+		user_id = id_user INTO songs_history;
+    RETURN songs_history;
+END $$
+
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
